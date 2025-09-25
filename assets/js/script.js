edc1c60ef0a3380c41c19f8531c5421a1e203f48
Kian Lu <kian901@gmail.com>
2025-09-25 15:49:20 +0800

    Adjust Window and add Taskbar code

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,27 @@ function updateTime() {
 }
 setInterval(updateTime, 1000);
 updateTime();
+
+// let offsetX, offsetY, currentWindow;
+
+// function dragStart(e, win) {
+//   currentWindow = win;
+//   offsetX = e.clientX - win.offsetLeft;
+//   offsetY = e.clientY - win.offsetTop;
+
+//   document.addEventListener("mousemove", dragMove);
+//   document.addEventListener("mouseup", dragEnd);
+// }
+
+// function dragMove(e) {
+//   if (!currentWindow) return;
+
+//   currentWindow.style.left = e.clientX - offsetX + "px";
+//   currentWindow.style.top = e.clientY - offsetY + "px";
+// }
+
+// function dragEnd() {
+//   document.removeEventListener("mousemove", dragMove);
+//   document.removeEventListener("mouseup", dragEnd);
+//   currentWindow = null;
+// }
